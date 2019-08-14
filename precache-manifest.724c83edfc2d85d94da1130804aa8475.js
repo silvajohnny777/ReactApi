5ea9440f656adc1f9abe750abac779a01dac2bfd
silvajohnny777 <jonatha_silva2012@hotmail.com>
2019-08-14 17:50:33 -0300

    Updates

---
+++ precache-manifest.724c83edfc2d85d94da1130804aa8475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313929de77e8605f84f8e32dd35d20d1",
+    "url": "/ReactApi/index.html"
+  },
+  {
+    "revision": "4b721491ff85a22c51c8",
+    "url": "/ReactApi/static/css/main.46486398.chunk.css"
+  },
+  {
+    "revision": "c540c8af81c49bc981bf",
+    "url": "/ReactApi/static/js/2.3f0407f5.chunk.js"
+  },
+  {
+    "revision": "4b721491ff85a22c51c8",
+    "url": "/ReactApi/static/js/main.8fb0525d.chunk.js"
+  },
+  {
+    "revision": "87fbc5174f108e808c9b",
+    "url": "/ReactApi/static/js/runtime~main.9c6158de.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-ApiRest/precache-manifest.f9b3ef90dd2344f7837b7100e808291a.js"
+  "/ReactApi/precache-manifest.724c83edfc2d85d94da1130804aa8475.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-ApiRest/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactApi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
